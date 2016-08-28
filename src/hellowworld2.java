e42b2dae27dd2b9211b69f30c2da26b0dad93a6b
IamUnInc <nick.r.matecki@gmail.com>
2016-08-27 23:08:57 -0500

    initial commit

---
+++ src/hellowworld2.java
@@ -0,0 +1,12 @@
+public class hellowworld2
+{
+	static String randomString = "This is totally a way to do this too!";
+	
+	static final double PINUP = 3.14;
+	
+	public static void main(String[] args)
+	{
+		System.out.println(PINUP);
+	}
+	
+}
